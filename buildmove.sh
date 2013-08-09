4689f4e78e9c0ed998c879988c8f54f5da0f809b
Rabi Alam <rabi@handshake-app.com>
2013-08-09 14:14:05 -0400

    [HS-2111] update swagger client: return overview
    - swagger client now returns overview in top level SwaggerApi model
      so it can be passed to swagger-ui views

--- buildmove.sh
+++ buildmove.sh
@@ -1,6 +1,13 @@
 export HS_STATIC=$HOME/code/work/handshake/Handshake/djserver/handshake/static
 
+export CWD=`pwd`
+
+cd $CWD/node_modules/swagger-client
+Cake bake
+cd $CWD
+
 Cake dist
 cp dist/swagger*.js $HS_STATIC/tastypie_swagger/js/
 cp dist/lib/*.js $HS_STATIC/tastypie_swagger/js/lib/
+cp node_modules/swagger-client/lib/swagger.js $HS_STATIC/tastypie_swagger/js/lib/
 cp dist/css/* $HS_STATIC/tastypie_swagger/css/
