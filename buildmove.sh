a95aea23acbaa98f09ef9e7c4a33161689b40ac5
Rabi Alam <rabi@handshake-app.com>
2013-08-16 11:59:39 -0400

    [HS-2099] revise build script to gen dirs

--- buildmove.sh
+++ buildmove.sh
@@ -6,6 +6,10 @@ cd $CWD/node_modules/swagger-client
 Cake bake
 cd $CWD
 
+mkdir -p $HS_STATIC/tastypie_swagger/
+mkdir -p $HS_STATIC/tastypie_swagger/js/lib/
+mkdir -p $HS_STATIC/tastypie_swagger/css/
+
 Cake dist
 cp dist/swagger*.js $HS_STATIC/tastypie_swagger/js/
 cp dist/lib/*.js $HS_STATIC/tastypie_swagger/js/lib/
